bd66ad4689a2d82559b4277a5d325034b4bd2a50
Jim Stuttard <jim.a.stuttard@gmail.com>
2017-05-17 15:17:18 +0100

    Annotated results.

--- src/Main.hs
+++ src/Main.hs
@@ -7,34 +7,34 @@ import LPTests
 
 main :: IO ()
 main = do
-  putStrLn "(1) Maximise x + 2y"
+  putStrLn "(1) Maximise x + 2y => ok"
   putStr "m1 x,y,p = "
   print $ res m1
-  putStrLn "(2) Maximise x + y"
+  putStrLn "(2) Maximise x + y => ok"
   putStr "m2 x,y,p = "
   print $ res m2
-  putStrLn "(3) Max 3x+y"  
-  putStr "m3 x,y,p = 3 vars and unrestricted not yet done! Ignore the rest!"
+  putStrLn "(3) Max 3x+y => WRONG!"  
+  putStr "m3 x,y,p = "
   print $ res m3
-  putStrLn "(4) Max 2x - 3y + 4z"
+  putStrLn "(4) Max 2x - 3y + 4z => WRNG"
   putStr "m4 x,y,z,p = "
   print $ res m4
-  putStrLn "(5) Max x + 2y - z"
+  putStrLn "(5) Max x + 2y - z => ok"
   putStr "m5 p,x,y,z,p = "
   print $ res m5 
-  putStrLn "(6) Max. 2x + y"
+  putStrLn "(6) Max. 2x + y => ok"
   putStr "m6 x,y,z,p = "
   print $ res m6 
   putStrLn "(7) Max. 2x - 2y + 4z"  
   putStrLn "m7 x,y,z,p = INFEASIBLE?"
 --  print $ res m7
-  putStrLn "(8) Max. 2x - 3y"
+  putStrLn "(8) Max. 2x - 3y => ok"
   print $ res m8
-  putStrLn "(9) Max. x + y"
+  putStrLn "(9) Max. x + y => ok"
   print $ res m9
-  putStrLn "(10) Max. 4x + y + z"
+  putStrLn "(10) Max. 4x + y + z => ok"
   print $ res m10
-  putStrLn "(11) Max. 2x + y"
+  putStrLn "(11) Max. 2x + y => ok"
   print $ res m11
   
 res = reverse . last . transpose . simplex

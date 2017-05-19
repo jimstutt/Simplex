6c916c85d6a107fc21b9e2c77a1121629e25ed0b
Jim Stuttard <jim.a.stuttard@gmail.com>
2017-05-19 10:29:31 +0100

    Commented out m4.

--- src/Main.hs
+++ src/Main.hs
@@ -18,9 +18,9 @@ main = do
   putStrLn "(3) Max 3x+y => ok"  
   putStr "m3 x,y,p = "
   print $ res m3
-  putStrLn "(4) Max 2x - 3y + 4z => ok"
-  putStr "m4 x,y,z,p = "
-  print $ res m4
+--  putStrLn "(4) Max 2x - 3y + 4z => ok"
+--  putStr "m4 x,y,z,p = "
+--  print $ res m4
   putStrLn "(5) Max x + 2y - z => ok"
   putStr "m5 p,x,y,z,p = "
   print $ res m5 
